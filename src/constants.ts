2bd482528b1c7afebb9f527f1378af693128a6c0
guanbinrui <guanbinrui@dimension.im>
2021-01-07 20:50:05 +0800

    :rocket: update GENESIS_TIMESTAMP

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const CHAIN_ID = 3;
 export const CONTRACT_ADDR = "0xF4C36D2a5C991E323d32F7d8D7EFe27cE71FfbaB";
 
-export const GENESIS_TIMESTAMP = 1606780800; // UNIX timestamp
+export const GENESIS_TIMESTAMP = 1609372800; // UNIX timestamp
 
 export const TOKEN_TYPE_ETHER = 0;
 export const TOKEN_TYPE_ERC20 = 1;
