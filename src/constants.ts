a29811ac66c546b3e55782872680f056eb45c3e6
guanbinrui <guanbinrui@dimension.im>
2021-01-07 20:26:00 +0800

    :rocket: upgrade contract

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const CHAIN_ID = 3;
-export const CONTRACT_ADDR = "0x0c2Fb8166419c6A81aA41e53515c0fcB676D1686";
+export const CONTRACT_ADDR = "0xF4C36D2a5C991E323d32F7d8D7EFe27cE71FfbaB";
 
 export const GENESIS_TIMESTAMP = 1606780800; // UNIX timestamp
 
