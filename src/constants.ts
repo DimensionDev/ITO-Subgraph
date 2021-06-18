6500fa886e5be6bf74109a58f15ef52ecaa72ed6
Hancheng Zhou <z308114274@gmail.com>
2021-06-18 16:56:04 +0800

    🔧 Little fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const CHAIN_ID = 56;
+export const CHAIN_ID = 137;
 export const TOKEN_TYPE_ETHER = 0;
 export const TOKEN_TYPE_ERC20 = 1;
-export { ERC20 } from "../generated/BSC_ITO/ERC20";
-export { ERC20NameBytes } from "../generated/BSC_ITO/ERC20NameBytes";
-export { ERC20SymbolBytes } from "../generated/BSC_ITO/ERC20SymbolBytes";
+export { ERC20 } from "../generated/POLYGON_ITO/ERC20";
+export { ERC20NameBytes } from "../generated/POLYGON_ITO/ERC20NameBytes";
+export { ERC20SymbolBytes } from "../generated/POLYGON_ITO/ERC20SymbolBytes";
 
